81b44076f6ec7cfbaf238cb3ebcd8eccc1ab0502
Victor <victorgson@outlook.com>
2022-12-20 16:53:36 +0100

    simple quiz react app using dotnet backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var [question, setQuestion] = useState();
+  var [answer, setAnswer] = useState();
+  var [loaded, setLoaded] = useState(false);
+
+  function postAnswer(answer) {
+    axios
+      .get(`https://localhost:7013/api/v1/TriviaQuiz/${question.id}-${answer}`)
+      .then((resp) => {
+        setAnswer(resp.data);
+        console.log(resp.data);
+      });
+  }
+
+  function getNewQuestion() {
+    axios.get("https://localhost:7013/api/v1/TriviaQuiz").then((resp) => {
+      setQuestion(resp.data);
+      console.log(resp.data);
+      setAnswer("");
+    });
+  }
+  useEffect(() => {
+    getNewQuestion();
+  }, []);
+
+  if (question != null) {
+    return (
+      <div className="App">
+        {
+          <div className="content">
+            <h1> {question.questionString} </h1>
+
+            <ul className="buttons">
+              {question.answers.map((a, index) => (
+                <button key={index} onClick={() => postAnswer(a)}>
+                  {a}
+                </button>
+              ))}
+            </ul>
+            <h1> {answer} </h1>
+            <button onClick={() => getNewQuestion()}> Next question </button>
+          </div>
+        }
+      </div>
+    );
+  } else {
+    return <div className="App">{<h1> Loading </h1>}</div>;
+  }
 }
 
 export default App;
